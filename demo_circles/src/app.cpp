75b7b9a30c1af1b459fbe87d83bf4fcd65b51082
bean <harry.bean.dev@gmail.com>
2023-09-12 17:51:45 +0000

    now logging into a csv file, minor changes

--- demo_circles/src/app.cpp
+++ demo_circles/src/app.cpp
@@ -1,50 +1,74 @@
 #include "app.h"
 
 #include <iostream>
+#include <fstream>
 #include <string>
-#include <cmath>
+#include <cstdint>
 
 #include "tef.h"
 
 #include "components.h"
 #include "systems.h"
+#include "utils.h"
 
+static std::ofstream log_file;
+
+// Log callback, writes to log_file in the CSV format
 void tef_log_cb(
     tef::log_level_t log_level,
     const std::string& world_name,
     std::thread::id thread_id,
     const std::string& message)
 {
-    std::ostringstream o;
-    const char* separator = " | ";
-    
+    // Replace " with "" because CSV
+    std::string world_name_copy = str_replace(world_name, "\"", "\"\"");
+    std::string message_copy = str_replace(message, "\"", "\"\"");
+
+    // time
+    log_file << '"' << str_from_time() << "\",";
+
+    // log_level
     switch (log_level)
     {
     case tef::log_level_t::error:
-        o << "error" << separator;
+        log_file << "error,";
         break;
-    case tef::log_level_t::warn:
-        o << "warn" << separator;
+    case tef::log_level_t::warning:
+        log_file << "warning,";
         break;
     case tef::log_level_t::info:
-        o << "info" << separator;
+        log_file << "info,";
         break;
     case tef::log_level_t::verbose:
-        o << "verbose" << separator;
+        log_file << "verbose,";
         break;
     default:
         break;
     }
 
-    o << world_name << separator;
-    o << thread_id << separator;
-    o << message;
+    // world_name, thread_id, message
+    log_file << '"' << world_name_copy << "\",";
+    log_file << thread_id << ',';
+    log_file << '"' << message_copy << "\"\n";
 
-    std::cout << o.str() << std::endl;
+    // Flush
+    log_file.flush();
 }
 
 void app_t::run()
 {
+    // Log file
+    log_file.open("./log.csv", std::ofstream::out | std::ofstream::trunc);
+    if (!log_file.is_open())
+    {
+        throw std::runtime_error("A log file could not be created/opened.");
+    }
+    else
+    {
+        log_file << "time,log_level,world_name,thread_id,message\n";
+        log_file.flush();
+    }
+
     // Create a world
     tef::world_t world("Circles", tef_log_cb);
 
@@ -69,9 +93,9 @@ void app_t::run()
     // Add systems
     {
         world.add_system(std::make_shared<s_movement>("Movement"));
-        //world.add_system(std::make_shared<s_circle_renderer>("Circle Renderer"));
+        world.add_system(std::make_shared<s_circle_renderer>("Circle Renderer"));
     }
 
     // Run
-    world.run(10, .5);
+    world.run(10, 5);
 }
