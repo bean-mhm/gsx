75f30e0cd496ea706a0735a5b76d82ce5762f6f2
bean <harry.bean.dev@gmail.com>
2023-09-13 05:05:48 +0000

    added log_entry_t, base_logger_t, ostream_logger_t, csv_logger_t

---
+++ tef/src/tef_log.cpp
@@ -0,0 +1,142 @@
+#include "tef_log.h"
+
+namespace tef
+{
+
+    log_entry_t::log_entry_t(
+        log_level_t log_level,
+        const std::string& world_name,
+        std::thread::id thread_id,
+        const std::string& message)
+        : log_level(log_level), world_name(world_name), thread_id(thread_id), message(message)
+    {}
+
+    base_logger_t::~base_logger_t()
+    {}
+
+    ostream_logger_t::ostream_logger_t(std::ostream& stream)
+        : stream(stream)
+    {
+        if (!stream)
+        {
+            throw std::runtime_error("The log output stream is in failure state.");
+        }
+    }
+
+    ostream_logger_t::~ostream_logger_t()
+    {}
+
+    void ostream_logger_t::log(const log_entry_t& entry)
+    {
+        std::scoped_lock(mutex);
+
+        if (!stream)
+        {
+            throw std::runtime_error("The log output stream is in failure state.");
+        }
+
+        // time
+        stream << str_format("%s | ", str_from_time().c_str());
+
+        // log_level
+        switch (entry.log_level)
+        {
+        case log_level_t::error:
+            stream << "E | ";
+            break;
+        case log_level_t::warning:
+            stream << "W | ";
+            break;
+        case log_level_t::info:
+            stream << "I | ";
+            break;
+        case log_level_t::verbose:
+            stream << "V | ";
+            break;
+        default:
+            break;
+        }
+
+        // world_name, thread_id, message
+        stream << str_format("%s | ", entry.world_name.c_str());
+        stream << entry.thread_id << " | ";
+        stream << entry.message << '\n';
+
+        // Flush
+        stream.flush();
+    }
+
+    csv_logger_t::csv_logger_t(const std::string& filename)
+        : filename(filename)
+    {
+        log_file.open(filename, std::ofstream::out | std::ofstream::trunc);
+        if (!log_file.is_open())
+        {
+            throw std::runtime_error(str_format("Log file \"%s\" could not be created/opened.", filename.c_str()));
+        }
+        else
+        {
+            log_file << "time,log_level,world_name,thread_id,message\n";
+            log_file.flush();
+        }
+    }
+
+    csv_logger_t::~csv_logger_t()
+    {}
+
+    void csv_logger_t::log(const log_entry_t& entry)
+    {
+        std::scoped_lock(mutex);
+
+        if (!log_file)
+        {
+            throw std::runtime_error(str_format("The log file stream \"%s\" is in failure state.", filename.c_str()));
+        }
+
+        // Replace " with "" because CSV
+        std::string world_name_copy = str_replace(entry.world_name, "\"", "\"\"");
+        std::string message_copy = str_replace(entry.message, "\"", "\"\"");
+
+        // time
+        log_file << '"' << str_from_time() << "\",";
+
+        // log_level
+        switch (entry.log_level)
+        {
+        case log_level_t::error:
+            log_file << "error,";
+            break;
+        case log_level_t::warning:
+            log_file << "warning,";
+            break;
+        case log_level_t::info:
+            log_file << "info,";
+            break;
+        case log_level_t::verbose:
+            log_file << "verbose,";
+            break;
+        default:
+            break;
+        }
+
+        // world_name, thread_id, message
+        log_file << '"' << world_name_copy << "\",";
+        log_file << entry.thread_id << ',';
+        log_file << '"' << message_copy << "\"\n";
+
+        // Flush
+        log_file.flush();
+    }
+
+    bool csv_logger_t::is_open() const
+    {
+        return log_file.is_open();
+    }
+
+    void csv_logger_t::close()
+    {
+        std::scoped_lock(mutex);
+        log_file.close();
+    }
+
+}
--- tef/src/tef_log.h
+++ tef/src/tef_log.h
@@ -1,7 +1,12 @@
 #pragma once
 
+#include <iostream>
+#include <fstream>
 #include <string>
-#include <functional>
+#include <thread>
+#include <mutex>
+
+#include "tef_utils.h"
 
 namespace tef
 {
@@ -18,17 +23,69 @@ namespace tef
     // Maximum log level to ever use (global)
     static constexpr log_level_t max_log_level = log_level_t::verbose;
 
-    // Callback function for logging
-    using cb_log_t = std::function<
-        void(
-            log_level_t log_level,
+    // Log entry
+    struct log_entry_t
+    {
+        log_level_t log_level;
+        std::string world_name;
+        std::thread::id thread_id;
+        std::string message;
+
+        log_entry_t(log_level_t log_level,
             const std::string& world_name,
             std::thread::id thread_id,
-            const std::string& message)
-    >;
+            const std::string& message);
+    };
+
+    // Abstract class for logging
+    // Note: The abstract function log() may be called from several threads simultaneously, so
+    // any derived class must manually implement synchronization.
+    class base_logger_t
+    {
+    public:
+        virtual ~base_logger_t();
+        virtual void log(const log_entry_t& entry) = 0;
+
+    };
+
+    // std::ostream logger
+    class ostream_logger_t : public base_logger_t
+    {
+    public:
+        std::ostream& stream;
+
+        ostream_logger_t(std::ostream& stream);
+        no_default_copy_move_constructor(ostream_logger_t);
+        virtual ~ostream_logger_t();
+
+        virtual void log(const log_entry_t& entry) override;
+
+    private:
+        std::mutex mutex;
+
+    };
+
+    // CSV logger
+    class csv_logger_t : public base_logger_t
+    {
+    public:
+        csv_logger_t(const std::string& filename);
+        no_default_copy_move_constructor(csv_logger_t);
+        virtual ~csv_logger_t();
+
+        virtual void log(const log_entry_t& entry) override;
+        bool is_open() const;
+        void close();
+
+    private:
+        const std::string filename;
+        std::ofstream log_file;
+        std::mutex mutex;
+
+    };
 
 }
 
 // Log macro (used internally)
 #define tef_log(log_level, world_name, message) if constexpr(log_level <= max_log_level) \
-cb_log(log_level, world_name, std::this_thread::get_id(), message)
+logger->log(log_entry_t(log_level, world_name, std::this_thread::get_id(), message))
