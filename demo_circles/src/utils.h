e6f554261f10082029ef293b74e14b3e24af43ce
bean <harry.bean.dev@gmail.com>
2023-09-13 03:57:11 +0000

    removed string helper functions

--- demo_circles/src/utils.h
+++ demo_circles/src/utils.h
@@ -1,12 +1,6 @@
 #pragma once
 
-#include <string>
-
 #include "glm/glm.hpp"
-
-void str_replace_in_place(std::string& s, const std::string& from, const std::string& to);
-std::string str_replace(const std::string& s, const std::string& from, const std::string& to);
-std::string str_from_time();
 
 glm::vec2 screen_to_uv(glm::uvec2 coord, glm::uvec2 res);
 float get_px2uv_ratio(glm::uvec2 res);
