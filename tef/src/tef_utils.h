3f35b61c699325f72c8d21d39d7780bc277ffc23
bean <harry.bean.dev@gmail.com>
2023-09-18 16:29:39 +0000

    tef_utils: added a new class worker_t

--- tef/src/tef_utils.h
+++ tef/src/tef_utils.h
@@ -3,6 +3,11 @@
 // STD
 #include <string>
 #include <vector>
+#include <deque>
+#include <functional>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
 #include <chrono>
 #include <cstdint>
 
@@ -81,5 +86,34 @@ namespace tef::utils
         std::chrono::steady_clock::time_point t_start,
         std::chrono::steady_clock::time_point t_end
     );
+
+    // Spawns a worker thread upon construction and keeps waiting for new jobs to process (using a
+    // FIFO queue). Upon deconstruction, the worker thread will automatically stop after the queue
+    // is empty.
+    class worker_t
+    {
+    public:
+        worker_t();
+        no_copy_move_constructor(worker_t);
+        ~worker_t();
+
+        // Enqueue a new job to be processed by the worker thread.
+        void enqueue(const std::function<void()>& job);
+
+        // Wait for the worker thread to process all jobs until the queue is empty.
+        void wait();
+
+    private:
+        std::jthread thread;
+
+        std::deque<std::function<void()>> jobs;
+        std::mutex jobs_mutex;
+
+        std::condition_variable cond_job_added;
+        std::condition_variable cond_queue_empty;
+
+        void loop(std::stop_token stop_token);
+
+    };
 
 }
