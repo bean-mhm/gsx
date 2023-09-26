a8f465ff34b96e33e99eadc70fe750d1fade60b1
bean <harry.bean.dev@gmail.com>
2023-09-26 18:25:12 +0000

    ray_t::to_string() is no longer constexpr

--- tef/src/math/ray.cpp
+++ tef/src/math/ray.cpp
@@ -3,6 +3,15 @@
 namespace tef::math
 {
 
+    std::string ray_t::to_string() const
+    {
+        return std::format(
+            "[o={}, d={}]",
+            o.to_string(),
+            d.to_string()
+        );
+    }
+
     std::ostream& operator<<(std::ostream& os, const ray_t& r)
     {
         os << r.to_string();
--- tef/src/math/ray.h
+++ tef/src/math/ray.h
@@ -26,14 +26,7 @@ namespace tef::math
         }
 
         // String
-        constexpr std::string to_string() const
-        {
-            return std::format(
-                "[o={}, d={}]",
-                o.to_string(),
-                d.to_string()
-            );
-        }
+        std::string to_string() const;
 
         // Print
         friend std::ostream& operator<<(std::ostream& os, const ray_t& r);
