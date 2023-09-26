2f10329b1cbae3a8acc75da78676c0cebcaa7aa2
bean <harry.bean.dev@gmail.com>
2023-09-26 18:27:17 +0000

    quaternion: some functions are no longer constexpr, other changes

--- tef/src/math/quaternion.cpp
+++ tef/src/math/quaternion.cpp
@@ -3,6 +3,51 @@
 namespace tef::math
 {
 
+    quaternion_t::quaternion_t(const mat3& m)
+    {
+        float mtrace = m(0, 0) + m(1, 1) + m(2, 2);
+        if (mtrace > 0.f)
+        {
+            // Compute w from matrix trace, then xyz
+            // 4w^2 = m(0, 0) + m(1, 1) + m(2, 2) + m(3, 3) (but m(3, 3) == 1)
+            float s = sqrt(mtrace + 1.f);
+            v.w = s / 2.f;
+            s = 0.5f / s;
+            v.x = (m(2, 1) - m(1, 2)) * s;
+            v.y = (m(0, 2) - m(2, 0)) * s;
+            v.z = (m(1, 0) - m(0, 1)) * s;
+        }
+        else
+        {
+            // Compute largest of $x$, $y$, or $z$, then remaining components
+            const int nxt[3] = { 1, 2, 0 };
+            float q[3];
+            int i = 0;
+            if (m(1, 1) > m(0, 0)) i = 1;
+            if (m(2, 2) > m(i, i)) i = 2;
+            int j = nxt[i];
+            int k = nxt[j];
+            float s = sqrt((m(i, i) - (m(j, j) + m(k, k))) + 1.f);
+            q[i] = s * 0.5f;
+            if (s != 0.f) s = 0.5f / s;
+            v.w = (m(k, j) - m(j, k)) * s;
+            q[j] = (m(j, i) + m(i, j)) * s;
+            q[k] = (m(k, i) + m(i, k)) * s;
+            v.x = q[0];
+            v.y = q[1];
+            v.z = q[2];
+        }
+    }
+
+    quaternion_t::quaternion_t(const mat4& m)
+        : quaternion_t(m.sub<3>())
+    {}
+
+    std::string quaternion_t::to_string() const
+    {
+        return v.to_string();
+    }
+
     std::ostream& operator<<(std::ostream& os, const quaternion_t& q)
     {
         os << q.to_string();
--- tef/src/math/quaternion.h
+++ tef/src/math/quaternion.h
@@ -25,51 +25,11 @@ namespace tef::math
             : v(v)
         {}
 
-        constexpr quaternion_t(const mat4& m)
-        {
-            float mtrace = m.m[0][0] + m.m[1][1] + m.m[2][2];
-            if (mtrace > 0.f)
-            {
-                // Compute w from matrix trace, then xyz
-                // 4w^2 = m[0][0] + m[1][1] + m[2][2] + m[3][3] (but m[3][3] == 1)
-                float s = sqrt(mtrace + 1.f);
-                v.w = s / 2.f;
-                s = 0.5f / s;
-                v.x = (m.m[2][1] - m.m[1][2]) * s;
-                v.y = (m.m[0][2] - m.m[2][0]) * s;
-                v.z = (m.m[1][0] - m.m[0][1]) * s;
-            }
-            else
-            {
-                // Compute largest of $x$, $y$, or $z$, then remaining components
-                const int nxt[3] = { 1, 2, 0 };
-                float q[3];
-                int i = 0;
-                if (m.m[1][1] > m.m[0][0]) i = 1;
-                if (m.m[2][2] > m.m[i][i]) i = 2;
-                int j = nxt[i];
-                int k = nxt[j];
-                float s = sqrt((m.m[i][i] - (m.m[j][j] + m.m[k][k])) + 1.f);
-                q[i] = s * 0.5f;
-                if (s != 0.f) s = 0.5f / s;
-                v.w = (m.m[k][j] - m.m[j][k]) * s;
-                q[j] = (m.m[j][i] + m.m[i][j]) * s;
-                q[k] = (m.m[k][i] + m.m[i][k]) * s;
-                v.x = q[0];
-                v.y = q[1];
-                v.z = q[2];
-            }
-        }
-
-        constexpr quaternion_t(const transform_t& t)
-            : quaternion_t(t.m)
-        {}
+        quaternion_t(const mat3& m);
+        quaternion_t(const mat4& m);
 
         // String
-        constexpr std::string to_string() const
-        {
-            return v.to_string();
-        }
+        std::string to_string() const;
 
         // Print
         friend std::ostream& operator<<(std::ostream& os, const quaternion_t& q);
@@ -132,26 +92,26 @@ namespace tef::math
             return quaternion_t(-v);
         }
 
-        // Generate a transform based on this quaternion
-        constexpr transform_t to_transform() const
+        // Generate a 3D transformation matrix based on this quaternion (left-handed)
+        constexpr mat4 to_transform() const
         {
             float xx = v.x * v.x, yy = v.y * v.y, zz = v.z * v.z;
             float xy = v.x * v.y, xz = v.x * v.z, yz = v.y * v.z;
             float wx = v.x * v.w, wy = v.y * v.w, wz = v.z * v.w;
 
-            mat4 m;
-            m.m[0][0] = 1 - 2 * (yy + zz);
-            m.m[0][1] = 2 * (xy + wz);
-            m.m[0][2] = 2 * (xz - wy);
-            m.m[1][0] = 2 * (xy - wz);
-            m.m[1][1] = 1 - 2 * (xx + zz);
-            m.m[1][2] = 2 * (yz + wx);
-            m.m[2][0] = 2 * (xz + wy);
-            m.m[2][1] = 2 * (yz - wx);
-            m.m[2][2] = 1 - 2 * (xx + yy);
+            mat4 r;
+            r(0, 0) = 1 - 2 * (yy + zz);
+            r(0, 1) = 2 * (xy + wz);
+            r(0, 2) = 2 * (xz - wy);
+            r(1, 0) = 2 * (xy - wz);
+            r(1, 1) = 1 - 2 * (xx + zz);
+            r(1, 2) = 2 * (yz + wx);
+            r(2, 0) = 2 * (xz + wy);
+            r(2, 1) = 2 * (yz - wx);
+            r(2, 2) = 1 - 2 * (xx + yy);
 
             // Transpose since we are left-handed
-            return transform_t(transpose(m), m);
+            return transpose(r);
         }
 
     };
