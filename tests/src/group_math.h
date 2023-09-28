46cea277996aaec03b69b1536bab49d81c62f31b
bean <harry.bean.dev@gmail.com>
2023-09-28 12:25:08 +0000

    added comment

--- tests/src/group_math.h
+++ tests/src/group_math.h
@@ -1,3 +1,5 @@
 #pragma once
 
+// Note: Not every function and/or class is tested here. If you want to make these tests complete,
+// feel free to do it in a separate branch and make a pull request.
 void test_group_math();
