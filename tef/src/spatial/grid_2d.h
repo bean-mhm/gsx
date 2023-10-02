9863329743bc5073bfa77eab904683ef899f42a6
bean <harry.bean.dev@gmail.com>
2023-10-02 10:35:22 +0000

    added new spatial data structures: grid_2d, hash_grid_2d

---
+++ tef/src/spatial/grid_2d.h
@@ -0,0 +1,88 @@
+#pragma once
+
+// STD
+#include <vector>
+#include <stdexcept>
+#include <cstdint>
+
+// Internal
+#include "../math.h"
+#include "../misc.h"
+
+namespace tef::spatial
+{
+
+    // 2D grid data structure
+    // Note: T must have a copy constructor.
+    // Note: T must have a field of type tef::math::vec2 named pos, representing the 2D position.
+    template<typename T>
+    class grid_2d_t
+    {
+    public:
+        const math::bounds2 bounds;
+        const math::ivec2 resolution;
+
+        grid_2d_t(math::bounds2 bounds, math::ivec2 resolution)
+            : bounds(bounds),
+            resolution(resolution),
+            cell_ratio(math::vec2(resolution) / bounds.diagonal())
+        {
+            if (math::min_component(resolution) < 1)
+                throw std::runtime_error("Grid resolution must be at least 1 in each dimension.");
+
+            containers.resize((size_t)resolution.x * (size_t)resolution.y);
+        }
+
+        no_default_construct(grid_2d_t);
+
+        void insert(const T& element)
+        {
+            math::ivec2 cell(math::floor(cell_ratio * (element.pos - bounds.pmin)));
+            cell.x = math::clamp(cell.x, 0, resolution.x - 1);
+            cell.y = math::clamp(cell.y, 0, resolution.y - 1);
+
+            size_t index = (size_t)cell.y * (size_t)resolution.x + (size_t)cell.x;
+            containers[index].push_back(element);
+        }
+
+        void query(const math::bounds2& range, std::vector<T*>& out_elements)
+        {
+            math::ivec2 start_cell(math::floor(cell_ratio * (range.pmin - bounds.pmin)));
+            start_cell.x = math::clamp(start_cell.x, 0, resolution.x - 1);
+            start_cell.y = math::clamp(start_cell.y, 0, resolution.y - 1);
+
+            math::ivec2 end_cell(math::floor(cell_ratio * (range.pmax - bounds.pmin)));
+            end_cell.x = math::clamp(end_cell.x, 0, resolution.x - 1);
+            end_cell.y = math::clamp(end_cell.y, 0, resolution.y - 1);
+
+            for (size_t y = start_cell.y; y <= end_cell.y; y++)
+            {
+                for (size_t x = start_cell.x; x <= end_cell.x; x++)
+                {
+                    size_t container_index = y * (size_t)resolution.x + x;
+                    for (auto& element : containers[container_index])
+                    {
+                        out_elements.push_back(&element);
+                    }
+                }
+            }
+        }
+
+        void query_all(std::vector<T*>& out_elements)
+        {
+            for (auto& container : containers)
+            {
+                for (auto& element : container)
+                {
+                    out_elements.push_back(&element);
+                }
+            }
+        }
+
+    private:
+        const math::vec2 cell_ratio;
+        std::vector<std::vector<T>> containers;
+
+    };
+
+}
---
+++ tef/src/spatial/hash_grid_2d.h
@@ -0,0 +1,98 @@
+#pragma once
+
+// STD
+#include <vector>
+#include <unordered_set>
+#include <stdexcept>
+#include <cstdint>
+
+// Internal
+#include "../math.h"
+#include "../misc.h"
+
+namespace tef::spatial
+{
+
+    struct bruh
+    {
+        math::vec2 pos;
+    };
+
+    // 2D hash grid data structure
+    // Note: T must have a copy constructor.
+    // Note: T must have a field of type tef::math::vec2 named pos, representing the 2D position.
+    template<typename T>
+    class hash_grid_2d_t
+    {
+    public:
+        const math::vec2 cell_size;
+
+        hash_grid_2d_t(math::vec2 cell_size, size_t n_containers)
+            : cell_size(cell_size)
+        {
+            if (math::min_component(cell_size) <= 0.0f)
+                throw std::runtime_error("Grid cell size must be positive.");
+
+            if (n_containers < 1)
+                throw std::runtime_error("Number of containers must be at least 1.");
+
+            containers.resize(n_containers);
+        }
+
+        no_default_construct(hash_grid_2d_t);
+
+        void insert(const T& element)
+        {
+            math::ivec2 cell(math::floor(element.pos / cell_size));
+            containers[get_container_index(cell)].push_back(element);
+        }
+
+        void query(const math::bounds2& range, std::vector<T*>& out_elements)
+        {
+            std::unordered_set<size_t> indices;
+
+            math::ivec2 start_cell(math::floor(range.pmin / cell_size));
+            math::ivec2 end_cell(math::floor(range.pmax / cell_size));
+
+            for (int32_t y = start_cell.y; y <= end_cell.y; y++)
+            {
+                for (int32_t x = start_cell.x; x <= end_cell.x; x++)
+                {
+                    size_t container_index = get_container_index(math::ivec2(x, y));
+                    indices.insert(container_index);
+                }
+            }
+
+            for (int index : indices)
+            {
+                std::vector<T>& container = containers[index];
+                for (auto& element : container)
+                {
+                    out_elements.push_back(&element);
+                }
+            }
+        }
+
+        void query_all(std::vector<T*>& out_elements)
+        {
+            for (auto& container : containers)
+            {
+                for (auto& element : container)
+                {
+                    out_elements.push_back(&element);
+                }
+            }
+        }
+
+    private:
+        std::vector<std::vector<T>> containers;
+
+        size_t get_container_index(math::ivec2 cell)
+        {
+            size_t hash = (size_t)math::abs((cell.x * 92837111) ^ (cell.y * 689287499));
+            return hash % containers.size();
+        }
+
+    };
+
+}
