676bdaa4a392fd04bb9d16db10f5fe04657bffd2
bean <harry.bean.dev@gmail.com>
2023-10-08 10:27:27 +0000

    spatial: renamed base_container_t to base_container_2d_t and added an abstract query() function that takes in a bounds2 range, added new abstract class base_container_3d_t

--- tef/src/internal_spatial/base_container.h
+++ tef/src/internal_spatial/base_container.h
@@ -5,16 +5,32 @@
 
 // Internal
 #include "../internal_common/all.h"
+#include "../internal_math/all.h"
 
 namespace tef::spatial
 {
 
     template<typename T>
-    class base_container_t
+    class base_container_2d_t
     {
     public:
         virtual usize size() const = 0;
         virtual bool insert(const T& element) = 0;
+        virtual void query(const math::bounds2& range, std::vector<T*>& out_elements) = 0;
+        virtual void query_all(std::vector<T*>& out_elements) = 0;
+        virtual void query_all(std::vector<T>& out_elements) const = 0;
+        virtual void clear() = 0;
+        virtual void rebuild() = 0;
+
+    };
+
+    template<typename T>
+    class base_container_3d_t
+    {
+    public:
+        virtual usize size() const = 0;
+        virtual bool insert(const T& element) = 0;
+        virtual void query(const math::bounds3& range, std::vector<T*>& out_elements) = 0;
         virtual void query_all(std::vector<T*>& out_elements) = 0;
         virtual void query_all(std::vector<T>& out_elements) const = 0;
         virtual void clear() = 0;
