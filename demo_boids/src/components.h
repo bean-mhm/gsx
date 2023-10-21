3f0d0337eaea282cd329814ecd9f51140db48c17
bean <harry.bean.dev@gmail.com>
2023-10-21 12:20:23 +0000

    demo_boids: added new component type c_attractor

--- demo_boids/src/components.h
+++ demo_boids/src/components.h
@@ -3,9 +3,16 @@
 // GSX
 #include "gsx/gsx.h"
 
-// Boid component
+// Boid
 struct c_boid
 {
     math::vec2 pos;
     math::vec2 vel;
+};
+
+// Attractor
+struct c_attractor
+{
+    math::vec2 pos;
+    f32 strength = 0;
 };
