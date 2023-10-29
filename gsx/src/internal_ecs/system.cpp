4ba7ef22d6a446019d77e156ff8121f6f2e55363
bean <harry.bean.dev@gmail.com>
2023-10-29 04:43:48 +0000

    ecs: added new struct execution_scheme_t, now using it instead of separate update_order and run_on_world_thread arguments, renamed run_on_caller_thread to run_on_world_thread

--- gsx/src/internal_ecs/system.cpp
+++ gsx/src/internal_ecs/system.cpp
@@ -5,10 +5,9 @@ namespace gsx::ecs
 
     base_system_t::base_system_t(
         const std::string& name,
-        i32 update_order,
-        bool run_on_caller_thread
+        const execution_scheme_t& exec_scheme
     )
-        : name(name), update_order(update_order), run_on_caller_thread(run_on_caller_thread)
+        : name(name), exec_scheme(exec_scheme)
     {}
 
     base_system_t::~base_system_t()
@@ -17,13 +16,17 @@ namespace gsx::ecs
     void base_system_t::on_start(world_t& world)
     {}
 
-    void base_system_t::on_update(world_t& world, const world_t::iter_t& iter)
+    void base_system_t::on_update(world_t& world, const iteration_t& iter)
     {}
 
-    void base_system_t::on_trigger(world_t& world, const world_t::iter_t& iter, const event_t& event)
+    void base_system_t::on_trigger(world_t& world, const iteration_t& iter, const event_t& event)
     {}
 
-    void base_system_t::on_stop(world_t& world, const world_t::iter_t& iter)
+    void base_system_t::on_stop(world_t& world, const iteration_t& iter)
+    {}
+
+    execution_scheme_t::execution_scheme_t(i32 update_order, bool run_on_world_thread)
+        : update_order(update_order), run_on_world_thread(run_on_world_thread)
     {}
 
 }
--- gsx/src/internal_ecs/system.h
+++ gsx/src/internal_ecs/system.h
@@ -13,6 +13,30 @@
 namespace gsx::ecs
 {
 
+    // Information on how a system must be run
+    struct execution_scheme_t
+    {
+        // Update order. A system with a higher order value will have its on_update() function
+        // invoked after a system with a lower order value. Systems with the same order will have
+        // their on_update() functions invoked in parallel.
+        // Note: The on_start() functions are invoked in the same order in which the systems were
+        // added to the world, while the on_stop() functions are invoked in reverse.
+        i32 update_order = 0;
+
+        // Force the parent world to invoke the abstract functions of a system on the same thread
+        // that is running the world. If this option is disabled, the system may or may not run in
+        // a separate thread. If it does so, all of its abstract functions will be invoked on that
+        // thread only. Otherwise, they will be invoked on the same thread that is running the
+        // world. This option is especially helpful when working with single-threaded contexts
+        // like in the OpenGL API.
+        // Note: If several systems with the same update order values have this option enabled,
+        // they will not be parallelized, as they all need to run on the same thread.
+        bool run_on_world_thread = false;
+
+        // Constructor
+        execution_scheme_t(i32 update_order, bool run_on_world_thread = false);
+    };
+
     // Abstract class for a system
     // Note: The abstract functions will be called at appropriate times by the parent world while
     // it is running.
@@ -22,24 +46,14 @@ namespace gsx::ecs
         // A name for the system
         const std::string name;
 
-        // Update order. A system with a higher order value will have its on_update() function
-        // invoked after a system with a lower order value. Systems with the same order will have
-        // their on_update() functions invoked in parallel.
-        // Note: Avoid modifying this value when the parent world is running.
-        i32 update_order = 0;
-
-        // Force the parent world to invoke the abstract functions of this system using the same
-        // thread that is running the world. If multiple systems with identical update orders have
-        // this option enabled, they will not be parallelized, but the other systems at their
-        // order will be updated together in parallel. This option is especially helpful when
-        // working with single-threaded contexts like in the OpenGL API.
-        bool run_on_caller_thread = false;
+        // Execution scheme
+        const execution_scheme_t exec_scheme;
 
         // Which event types will trigger this system?
         std::set<event_type_t> triggers;
 
         // Create a system with given properties.
-        base_system_t(const std::string& name, i32 update_order, bool run_on_caller_thread);
+        base_system_t(const std::string& name, const execution_scheme_t& exec_scheme);
         no_default_copy_construct_no_assignment(base_system_t);
         virtual ~base_system_t();
 
@@ -51,7 +65,7 @@ namespace gsx::ecs
         // Called when triggered by an event.
         // Note: Avoid starting separate threads that keep running after returning from this
         // function.
-        virtual void on_trigger(world_t& world, const world_t::iter_t& iter,
+        virtual void on_trigger(world_t& world, const iteration_t& iter,
             const event_t& event);
 
         // Called in every iteration when the world is running. A system would typically get a
@@ -59,13 +73,13 @@ namespace gsx::ecs
         // iteration can be manually parallelized by the user for improved performance.
         // Note: Avoid starting separate threads that keep running after returning from this
         // function.
-        virtual void on_update(world_t& world, const world_t::iter_t& iter);
+        virtual void on_update(world_t& world, const iteration_t& iter);
 
         // Called when the world stops running, in the opposite order to that in which the systems
         // were added.
         // Note: Avoid starting separate threads that keep running after returning from this
         // function.
-        virtual void on_stop(world_t& world, const world_t::iter_t& iter);
+        virtual void on_stop(world_t& world, const iteration_t& iter);
 
     };
 
